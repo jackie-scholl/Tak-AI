abe2e5a9890381ec6a10ca65526faddaa653be99
SawyerWelden <sawyer.welden15@ncf.edu>
2017-01-17 12:58:58 -0500

    tps input

--- src/foogame/TPSInput.java
+++ src/foogame/TPSInput.java
@@ -1,10 +1,18 @@
 package foogame;
 
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
 public class TPSInput {
 	private String tps;
 	private String[] grid;
 	private int turnIndicator;
 	private int turnNumber;
+	private int whitePieces;
+	private int blackPieces;
+	private int whiteCapstones;
+	private int blackCapstones;
 	private boolean print_verification = true;
 
 	public TPSInput(String tps) {
@@ -12,25 +20,74 @@ public class TPSInput {
 			print_verification = false;
 		}
 		this.tps = tps;
+		this.whitePieces = 21; // TODO change for 6x6
+		this.blackPieces = 21; // TODO change for 6x6
+		this.whiteCapstones = 1;
+		this.blackCapstones = 1;
 	}
 
 	public Board populateBoard() {
 		if (verifyTPS(this.tps)) {
 			if (print_verification) {
-				System.out.println("tps was verified:");
+				System.out.println("tps was verified:" + tps);
 				for (String i : grid) {
 					System.out.println(i);
 				}
 				System.out.println("Turn Indicator: " + turnIndicator);
 				System.out.println("Turn Number: " + turnNumber);
 			}
-			// make the board
-			
-			
+			Stack[][] out = new Stack[5][5];
+			int row = 0;
+			for (String i : grid) {
+				String[] eachSpace = i.split(i);
+				int col = 0;
+				for (String space : eachSpace) {
+					out[row][col] = buildSpace(space);
+					col++;
+				}
+				row++;
+			}
+			Color turnColor = turnIndicator == 1 ? Color.WHITE : Color.BLACK;
+			EnumMap<Color, Integer> numStones = new EnumMap<Color, Integer>(Color.class);
+			numStones.put(Color.WHITE, this.whitePieces);
+			numStones.put(Color.BLACK, this.blackPieces);
+			EnumMap<Color, Integer> numCapstones = new EnumMap<Color, Integer>(Color.class);
+			numCapstones.put(Color.WHITE, this.whiteCapstones);
+			numCapstones.put(Color.BLACK, this.blackCapstones);
+			//return new Board(out, numStones, numCapstones, turnColor, turnNumber);
 			return new Board(5);
 		}
-		System.out.println(tps + "failed");
+		System.err.println(tps + "failed");
 		return new Board(5);
+	}
+
+	private Stack buildSpace(String in) {
+		List<Stone> stones = new ArrayList<Stone>();
+		char[] chars = in.toCharArray();
+		for (char c : chars) {
+			if (c == 'X' || c == 'x') {
+				break;
+			} else if (c == '1') {
+				stones.add(new Stone(PieceType.FLAT, Color.WHITE));
+				this.whitePieces--;
+			} else if (c == '2') {
+				stones.add(new Stone(PieceType.FLAT, Color.BLACK));
+				this.blackPieces--;
+			} else if (c == 'C' || c == 'c') {
+				stones.remove(stones.get(stones.size() - 1));
+				stones.add(new Stone(PieceType.CAPSTONE, stones.get(stones.size() - 1).color));
+				if (stones.get(stones.size() - 1).color == Color.WHITE) {
+					this.whiteCapstones--;
+				} else {
+					this.blackCapstones--;
+				}
+			} else if (c == 'S' || c == 's') {
+				stones.remove(stones.get(stones.size() - 1));
+				stones.add(new Stone(PieceType.WALL, stones.get(stones.size() - 1).color));
+			}
+		}
+		Stone[] stoneArr = stones.toArray(new Stone[stones.size()]);
+		return new Stack(stoneArr);
 	}
 
 	private boolean verifyTPS(String in) {
@@ -60,11 +117,8 @@ public class TPSInput {
 			this.turnIndicator = Integer.parseInt(turnIndicator);
 		}
 		String turnNum = str.substring(3).replace("]", "");
-		if (!turnNum.matches("\\d")) {
-			System.out.println("Bad turn number: " + turnNum);
-		} else {
-			this.turnNumber = Integer.parseInt(turnNum);
-		}
+
+		this.turnNumber = Integer.parseInt(turnNum);
 
 		return true;
 	}
