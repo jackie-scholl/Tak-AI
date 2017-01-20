f3518527e344adc99e9f79ee1cbaea031e2b623b
sawyerwelden15 <sawyer.welden@gmail.com>
2017-01-20 12:04:02 -0500

    heurisitc 2 subtracted rather than added

--- src/foogame/Heuristics.java
+++ src/foogame/Heuristics.java
@@ -7,33 +7,29 @@ import java.util.function.BiFunction;
 
 public class Heuristics {
 	public static final Map<Integer, BiFunction<Board, Color, Double>> HEURISTIC_MAP = new HashMap<>();
-	
+
 	static {
 		HEURISTIC_MAP.put(0, Heuristics::heuristic0);
 		HEURISTIC_MAP.put(1, Heuristics::heuristic1);
 		HEURISTIC_MAP.put(2, Heuristics::heuristic2);
-		//System.out.println("setting heuristic map");
-		//System.out.println(HEURISTIC_MAP);
+		// System.out.println("setting heuristic map");
+		// System.out.println(HEURISTIC_MAP);
 	}
-	
+
 	// flat count
 	private static double featureNumStonesOnBoard(Board b, Color col) {
 		return b.numStonesOnBoard(col);
 	}
-	
+
 	// num stones we've played
 	private static double featureNumStones(Board b, Color col) {
 		return b.getNumStones(col);
 	}
-	
+
 	// closeness of our stones
 	private static double featureClustering(Board b, Color col) {
-		return Position.positionStream(b.size)
-				.filter(p -> Minimaxer.isColor(b, p, col))
-				.mapToLong(p -> Arrays.stream(Direction.values())
-						.map(d -> p.move(d))
-						.filter(p2 -> Minimaxer.isColor(b, p2, col))
-						.count())
+		return Position.positionStream(b.size).filter(p -> Minimaxer.isColor(b, p, col)).mapToLong(p -> Arrays
+				.stream(Direction.values()).map(d -> p.move(d)).filter(p2 -> Minimaxer.isColor(b, p2, col)).count())
 				.sum();
 	}
 
@@ -45,13 +41,13 @@ public class Heuristics {
 	public static double heuristic1(Board b, Color col) {
 		double a1 = featureNumStonesOnBoard(b, col);
 		double a2 = featureNumStones(b, col);
-		return (a1 / 200) + (a2 / 200);
+		return (a1 / 200) + (a2 / -200);
 	}
 
 	public static double heuristic2(Board b, Color col) {
 		double a1 = featureNumStonesOnBoard(b, col);
 		double a2 = featureNumStones(b, col);
 		double a3 = featureClustering(b, col);
-		return a1/200 + a2/200 + a3/400;
+		return a1 / 200 + a2 / -200 + a3 / 400;
 	}
 }
