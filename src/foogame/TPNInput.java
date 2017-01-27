adcc94c6bf75075520de5482ed0c3f14b00e86d7
sawyerwelden15 <sawyer.welden@gmail.com>
2017-01-27 13:21:26 -0500

    oops

--- src/foogame/TPNInput.java
+++ src/foogame/TPNInput.java
@@ -89,14 +89,14 @@ public class TPNInput {
 			}
 			// make the move and update the board
 			Move mWhite = simMove(b, whiteMove).get();
-			 System.out.printf("White Move: %s%n", mWhite.ptn());
+			// System.out.printf("White Move: %s%n", mWhite.ptn());
 			b = BoardMoveImpl.makeMove(b, mWhite);
 			//System.out.println(whiteWin + calculateFeatureScores(b, Color.WHITE, turn));
 			out.add(whiteWin + calculateFeatureScores(b, Color.WHITE, turn));
 
 			if (blackMove.length() > 1) {
 				Move mBlack = simMove(b, blackMove).get();
-				 System.out.printf("Black Move: %s%n", mBlack.ptn());
+				// System.out.printf("Black Move: %s%n", mBlack.ptn());
 				b = BoardMoveImpl.makeMove(b, mBlack);
 				//System.out.println(blackWin + calculateFeatureScores(b, Color.BLACK, turn));
 				out.add(blackWin + calculateFeatureScores(b, Color.BLACK, turn));
