f7292f823014b48b321bcd18938c5101f4c4fe69
SawyerWelden <sawyer.welden15@ncf.edu>
2017-01-26 23:53:55 -0500

    tpn input for linear regression nearly finished

--- src/foogame/TPNInput.java
+++ src/foogame/TPNInput.java
@@ -9,7 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class TPNInput {
@@ -30,25 +35,54 @@ public class TPNInput {
 		String line = in.readLine();
 		while (line != null) {
 			// process out lines and stuff
-			if (line.length() != 0 && line.charAt(0) != '[') {
+			if (line.length() != 0 && line.charAt(0) != '[' && line.charAt(0) != ' ') {
 				lines.add(line);
 			}
 			line = in.readLine();
 		}
 		doMovesAndThings(lines);
+		in.close();
 	}
-	
+
 	private static void doMovesAndThings(List<String> lines) {
-		for (String line : lines) {
+		Board b0 = new Board(5); // there has to be a better way to
+		Board b = new Board(b0.getBoardArray()); // do this but im not seeing it
+		Map <Board, Double> whiteMoveScoreMap = new LinkedHashMap<Board, Double>();
+		Map <Board, Double> blackMoveScoreMap = new LinkedHashMap<Board, Double>();
+		BiFunction<Board, Color, Double> heuristic = Heuristics.HEURISTIC_MAP.get(3); //change heuristic num here
+		System.out.println(lines.size());
+		for (String lineFull : lines) {
 			// removes the "1. " part of the move line
-			line = line.substring(line.indexOf(" ")+1);
-			//System.out.println(line);
+			String turnNumString = lineFull.substring(0, lineFull.indexOf(" "));
+			String line = lineFull.substring(lineFull.indexOf(" ") + 1);
 			String whiteMove = line.substring(0, line.indexOf(" "));
-			String blackMove = line.substring(line.indexOf(" ")+1);
-			System.out.printf("White's Move: %s%n", whiteMove);
-			System.out.printf("Blacks's Move: %s%n", blackMove);
+			String blackMove = line.substring(line.indexOf(" ") + 1);
+			//System.out.printf("White's Move: %s%n", whiteMove);
+			//System.out.printf("Blacks's Move: %s%n", blackMove);
+			// make the move and update the board
+			System.out.println(turnNumString);
+			Move mWhite = simMove(b, whiteMove).get();
+			System.out.println("Move is: " + mWhite.ptn());
+			b = BoardMoveImpl.makeMove(b, mWhite);
+			Move mBlack = simMove(b, blackMove).get();
+			System.out.println("Move is: " + mBlack.ptn());
+			b = BoardMoveImpl.makeMove(b, mBlack);
+			
+			//System.out.printf("WhiteMove Heuristic: %f", heuristic.apply(b, Color.WHITE));
+
 		}
-		//System.out.println(lines.size());
+	}
+
+	public static Optional<Move> simMove(Board board, String moveTPN) {
+		Optional<Move> m = PTNInput.parse(moveTPN, board.whoseTurn);
+		if (!m.isPresent()) {
+			return m;
+		}
+		if (!board.isLegalMove(m.get())) {
+			System.err.printf("Illegal move: %s%n", moveTPN);
+			return Optional.empty();
+		}
+		return m;
 	}
 
 }
