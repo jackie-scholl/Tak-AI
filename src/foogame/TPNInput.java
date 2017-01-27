9a25ded757c795b242d4315a39a518a8c3eabec5
sawyerwelden15 <sawyer.welden@gmail.com>
2017-01-27 13:07:54 -0500

    all saves to files

--- src/foogame/TPNInput.java
+++ src/foogame/TPNInput.java
@@ -1,8 +1,10 @@
 package foogame;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,21 +21,26 @@ public class TPNInput {
 		// List<File> filesInFolder =
 		// Files.walk(Paths.get("D:\\Workspace\\Tak\\IncomingPTN")).filter(Files::isRegularFile)
 		// .map(Path::toFile).collect(Collectors.toList());
-
+		BufferedWriter bfOut = new BufferedWriter(new FileWriter("FeatureScoring.ssv"));
 		for (File f : filesToProcess) {
 			System.out.println(f.toString());
-			processFile(f);
+			List<String> lines = processFile(f);
+			for (String line : lines) {
+				bfOut.append(line);
+				bfOut.newLine();
+			}
 		}
+		bfOut.flush();
 	}
 
-	private static void processFile(File f) throws IOException {
+	private static List<String> processFile(File f) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader(f));
 		List<String> lines = new ArrayList<String>();
 		String line = in.readLine();
 		Color winner = Color.WHITE;
 		while (line != null) {
 			// process out lines and stuff
-			
+
 			if (line.contains("Result")) {
 				String res = line.substring(line.indexOf("\"") + 1, line.indexOf("\"") + 4);
 				if (res.indexOf("0") == 0) {
@@ -45,13 +52,15 @@ public class TPNInput {
 			}
 			line = in.readLine();
 		}
-		doMovesAndThings(lines, winner);
 		in.close();
+		return doMovesAndThings(lines, winner);
+
 	}
 
-	private static void doMovesAndThings(List<String> lines, Color winner) {
+	private static List<String> doMovesAndThings(List<String> lines, Color winner) {
 		Board b0 = new Board(5); // there has to be a better way to
 		Board b = new Board(b0.getBoardArray());
+		List<String> out = new ArrayList<String>();
 
 		for (String lineFull : lines) {
 			// removes the "1. " part of the move line
@@ -72,22 +81,23 @@ public class TPNInput {
 			int turn = Integer.parseInt(turnNumString.substring(0, turnNumString.indexOf(".")));
 			String whiteWin = winner == Color.WHITE ? "1 " : "-1 ";
 			String blackWin = winner == Color.BLACK ? "1 " : "-1 ";
-			
+
 			// make the move and update the board
 			Move mWhite = simMove(b, whiteMove).get();
 			// System.out.printf("White Move: %s%n", mWhite.ptn());
 			b = BoardMoveImpl.makeMove(b, mWhite);
-			System.out.println(whiteWin + calculateFeatureScores(b, Color.WHITE, turn));
-			
-			
+			//System.out.println(whiteWin + calculateFeatureScores(b, Color.WHITE, turn));
+			out.add(whiteWin + calculateFeatureScores(b, Color.WHITE, turn));
+
 			if (blackMove.length() > 1) {
 				Move mBlack = simMove(b, blackMove).get();
 				// System.out.printf("Black Move: %s%n", mBlack.ptn());
 				b = BoardMoveImpl.makeMove(b, mBlack);
-				System.out.println(blackWin + calculateFeatureScores(b, Color.BLACK, turn));
+				//System.out.println(blackWin + calculateFeatureScores(b, Color.BLACK, turn));
+				out.add(blackWin + calculateFeatureScores(b, Color.BLACK, turn));
 			}
-
 		}
+		return out;
 	}
 
 	public static String calculateFeatureScores(Board b, Color col, int turn) {
@@ -98,7 +108,7 @@ public class TPNInput {
 
 		int[] featuresWeWant = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 		for (int i : featuresWeWant) {
-			double f = h.get(i).apply(b, col);
+			double f = h.get(i).apply(b, col) - h.get(i).apply(b, col.other());
 			out.append(f + " ");
 		}
 
