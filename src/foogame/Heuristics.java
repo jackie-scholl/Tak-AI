01a42bed531d3ebd84bd72f9d1ae5b1e8eb92577
Jackie Scholl <jackie.h.scholl@gmail.com>
2017-01-27 11:17:00 -0500

    Small fix

--- src/foogame/Heuristics.java
+++ src/foogame/Heuristics.java
@@ -31,6 +31,10 @@ public class Heuristics {
 		FEATURE_MAP.put(8,  Heuristics::featureCapstoneHard);
 	}
 	
+	private Heuristics() {
+		// prevent accidental instantiation
+	}
+	
 	public Map<Integer, BiFunction<Board, Color, Double>> getFeatureMap() {
 		return FEATURE_MAP;
 	}
